b3cdf361340b0147f791c55c72e8bd7edcc0aaa5
Giulio Dioguardi <giulio.d52@gmail.com>
2021-10-13 23:02:13 +0200

    Fixed running solve.py and profile_all.py profile.py got an import loop, renaming it, fixes that.

--- profile_all.py
+++ profile_all.py
@@ -7,9 +7,9 @@ from solve import solve
 from factory import SolverFactory
 
 methods = [
-    "astar",
-    # "breadthfirst",
-    # "depthfirst",
+    # "astar",
+    "breadthfirst",
+    "depthfirst",
     "dijkstra",
     # "leftturn",
 ]
@@ -32,7 +32,7 @@ def profile():
     for m in methods:
         for i in inputs:
             with tempfile.NamedTemporaryFile(suffix='.png') as tmp:
-                solve(SolverFactory(), m, "examples/%s.png" % i, tmp.name)
+                solve(SolverFactory(), m, "examples/%s.png" % i, tmp)
 
 profiler = BProfile('profiler.png')
 with profiler:
